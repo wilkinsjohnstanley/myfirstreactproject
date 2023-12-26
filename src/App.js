4555769c2a39cb6b1aebff75647d158aedd8c871
John Wilkins <wilkinsjohnstanley@gmail.com>
2023-12-26 15:50:43 -0600

    Props In Functional Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Profile from './components/Profile'
+
 function App() {
   return (
     <div className="App">
       <Hello></Hello>
       <Message></Message>
+      <Profile name = "John"></Profile>
+      <Profile name = "Stanley"></Profile>
+      <Profile name = "Wilkins"></Profile>
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,5 @@
+function Profile(props){
+    console.log(props)
+    return <h1>Name: {props.name}</h1>
+}
+export default Profile;
