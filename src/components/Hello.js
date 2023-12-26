ef67d7aacc43159afe29a5f1c7b59e08e5297983
John Wilkins <wilkinsjohnstanley@gmail.com>
2023-12-26 15:42:37 -0600

    3:42PMTuesday commit

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -3,6 +3,13 @@ function Hello(){
     return <h1>こんにちは、世界！ジョンと申します</h1>
 }
 */
-const Hello = ()=> <h1>こんにちは、世界！ジョンと申します</h1>
 
+
+const name = "ジョン";
+
+function Hello(){
+    return <h1>皆さん、こんにちは！
+         {name} と申します </h1>
+}
 export default Hello;
+
