bae4aecbc50c131b67bbdd643031841963474a61
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-01-06 16:25:55 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Profile from './components/Profile'
+import Hello from './components/Hello';
+import Message from './components/Message';
+import Profile from './components/Profile';
 import Counter from './components/Counter';
 import Resume from './components/Resume';
-import FunctionEvent from './components/FunctionEvent'
+import FunctionEvent from './components/FunctionEvent';
 function App() {
   return (
     <div className="App">
